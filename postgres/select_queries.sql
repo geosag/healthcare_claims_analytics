c43acb3ce895c02c6835b0df4cd2b53824dd6092
geosag <geo.sagris@gmail.com>
2026-07-03 12:57:08 +0300

    Upper SQL keywords in sql_queries file

--- postgres/select_queries.sql
+++ postgres/select_queries.sql
@@ -1,76 +1,76 @@
 -- cleaned st table
-select 
+SELECT 
 	status_id,
-	initcap(claim_status) as claim_status,
-	upper("type") as "type"
-from st;
+	INITCAP(claim_status) AS claim_status,
+	UPPER("type") AS "type"
+FROM st;
 
 -- cleaned claim table
-select 
+SELECT 
 	claim_id,
 	status_id, 
-	trim(date_of_service)::date as date_of_service,
-	date(date_trunc('month', date(trim(date_of_service)))) as dos_month_year,
-	extract(year from trim(date_of_service)::date) as dos_year,
-	trim(received_date)::date as received_date,
-	date(date_trunc('month', date(trim(received_date)))) as received_date_month_year,
-	extract(year from trim(received_date)::date) as received_date_year,
+	TRIM(date_of_service)::DATE AS date_of_service,
+	DATE(DATE_TRUNC('month', DATE(TRIM(date_of_service)))) AS dos_month_year,
+	EXTRACT(YEAR FROM TRIM(date_of_service)::DATE) AS dos_year,
+	TRIM(received_date)::DATE AS received_date,
+	DATE(DATE_TRUNC('month', DATE(TRIM(received_date)))) AS received_date_month_year,
+	EXTRACT(YEAR FROM TRIM(received_date)::DATE) AS received_date_year,
 	add_by
-from claim;
+FROM claim;
 
 -- cleaned cov table
-select distinct coverage_name from (
-select
+SELECT DISTINCT coverage_name FROM (
+SELECT
 	coverage_id,
 	member_id,
-	initcap(lower(trim(coverage_name))) as coverage_name,
-	trim(effective_date)::date as effective_date,
-	case
-		when trim(term_date) = 'NULL' then null
-		else trim(term_date)::date
-	end as term_date
-from cov);
+	INITCAP(LOWER(TRIM(coverage_name))) AS coverage_name,
+	TRIM(effective_date)::DATE AS effective_date,
+	CASE
+		WHEN TRIM(term_date) = 'NULL' THEN NULL
+		ELSE TRIM(term_date)::DATE
+	END AS term_date
+FROM cov);
 
 -- cleaned add table
-select * from add;
+SELECT * FROM add;
 
 -- cleaned claim_payment table
-select * from claim_payment;
+SELECT * FROM claim_payment;
 
 -- cleaned mem table
-select
+SELECT
 	member_id,
 	member_first_name,
 	member_last_name,
 	address_id,
-	trim(gender) as gender,
-	date(split_part(member_dob, '-', 3) || '-' || split_part(member_dob, '-', 2) || '-' || split_part(member_dob, '-', 1)) as member_dob,
-	case 
-		when (current_date - date(split_part(member_dob, '-', 3) || '-' || split_part(member_dob, '-', 2) || '-' || split_part(member_dob, '-', 1)))/365 between 0 and 18
-			then '0-18'
-		when (current_date - date(split_part(member_dob, '-', 3) || '-' || split_part(member_dob, '-', 2) || '-' || split_part(member_dob, '-', 1)))/365 between 19 and 35
-			then '19-35'
-		when (current_date - date(split_part(member_dob, '-', 3) || '-' || split_part(member_dob, '-', 2) || '-' || split_part(member_dob, '-', 1)))/365 between 36 and 50
-			then '36-50'
-		else '50+'
-	end as age_bins,
+	TRIM(gender) AS gender,
+	DATE(SPLIT_PART(member_dob, '-', 3) || '-' || SPLIT_PART(member_dob, '-', 2) || '-' || SPLIT_PART(member_dob, '-', 1)) AS member_dob,
+	CASE 
+		WHEN (CURRENT_DATE - DATE(SPLIT_PART(member_dob, '-', 3) || '-' || SPLIT_PART(member_dob, '-', 2) || '-' || SPLIT_PART(member_dob, '-', 1)))/365 BETWEEN 0 AND 18
+			THEN '0-18'
+		WHEN (CURRENT_DATE - DATE(SPLIT_PART(member_dob, '-', 3) || '-' || SPLIT_PART(member_dob, '-', 2) || '-' || SPLIT_PART(member_dob, '-', 1)))/365 BETWEEN 19 AND 35
+			THEN '19-35'
+		WHEN (CURRENT_DATE - DATE(SPLIT_PART(member_dob, '-', 3) || '-' || SPLIT_PART(member_dob, '-', 2) || '-' || SPLIT_PART(member_dob, '-', 1)))/365 BETWEEN 36 AND 50
+			THEN '36-50'
+		ELSE '50+'
+	END AS age_bins,
 	claim_id,
 	coverage_id
-from mem;
+FROM mem;
 
 -- cleaned pr table
-select 
+SELECT 
 	provider_id,
 	provider_first_name,
 	provider_last_name,
-	trim(degree) as degree,
-	trim(network) as network,
+	TRIM(degree) AS degree,
+	TRIM(network) AS network,
 	claim_id,
-	case 
-		when trim(practice_name) = 'ABC HOSPITAL'
-		then 'ABC Hospital'
-		else initcap(lower(trim(practice_name)))
-	end as practice_name,
+	CASE 
+		WHEN TRIM(practice_name) = 'ABC HOSPITAL'
+		THEN 'ABC Hospital'
+		ELSE INITCAP(LOWER(TRIM(practice_name)))
+	END AS practice_name,
 	address_id,
-	trim(gender) as gender
-from pr;
+	TRIM(gender) AS gender
+FROM pr;
